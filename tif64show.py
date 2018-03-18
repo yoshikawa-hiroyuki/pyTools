6b12a822caf402b3a78f9c0c77b1fd6b6c0561d2
Yoshikawa Hiroyuki <yoh@yohair.local>
2018-03-18 23:40:48 +0900

    add slice argument to tif64show

--- tif64show.py
+++ tif64show.py
@@ -12,12 +12,26 @@ except:
   sys.exit(1)
 
 if len(sys.argv) < 2:
-  print ("usage: %s file.tif")
+  print ("usage: %s file.tif [slice]" % sys.argv[0])
   sys.exit(2)
+if sys.argv[1] in ['-h', '--help']:
+  print ("usage: %s file.tif [slice]" % sys.argv[0])
+  sys.exit(0)
+
+slice = 0
+if len(sys.argv) > 2:
+  try:
+    slice = int(sys.argv[2])
+  except:
+    pass
 
 img = tifffile.imread(sys.argv[1])
 if len(img.shape) == 3:
-  plt.imshow(img[0])
+  if slice >= img.shape[0]:
+    print ("Warn: specified slice geater than #of TIFF slice, showing 0")
+    plt.imshow(img[0])
+  else:
+    plt.imshow(img[slice])
 elif len(img.shape) == 2:
   plt.imshow(img)
 else:
